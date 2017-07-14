c6667733177b2586200437dbf904b48ee07fb692
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2017-07-14 16:11:21 +0200

    First commit

---
+++ src/containers/Root/Root.dev.js
@@ -0,0 +1,46 @@
+import has from 'lodash/has';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { Provider } from 'react-redux';
+// import { Router, RouterContext } from 'react-router';
+import DevTools from '../DevTools/DevTools';
+import GoogleAnalytics from 'react-ga';
+import { StaticRouter, BrowserRouter } from 'react-router'
+
+export default class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.onUpdate = this.onUpdate.bind(this);
+  }
+
+  onUpdate() {
+    const { store, type } = this.props;
+    if (type !== 'server') {
+      const state = store.getState();
+      if (has(state, 'router.pathname')) {
+        GoogleAnalytics.pageview(state.router.pathname);
+      }
+    }
+  }
+  render() {
+    const { store, history, routes, type, renderProps } = this.props;
+    return (
+      <Provider store={store}>
+        <div>
+          {type === 'server'
+            ? <StaticRouter {...renderProps} />
+            : <BrowserRouter history={history} routes={routes} onUpdate={this.onUpdate} />}
+          <DevTools />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+  routes: PropTypes.node.isRequired,
+  type: PropTypes.string,
+  renderProps: PropTypes.object
+};
---
+++ src/server.js
@@ -0,0 +1,131 @@
+import Express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import favicon from 'serve-favicon';
+import compression from 'compression';
+import http from 'http';
+import proxy from 'express-http-proxy';
+import path from 'path';
+import url from 'url';
+import { match, createMemoryHistory } from 'react-router';
+import _ from 'lodash';
+
+import { Provider } from 'react-redux';
+import { StaticRouter, matchPath } from 'react-router'
+
+import config from './config';
+import configureStore from './store/configureStore';
+import Html from './helpers/Html';
+import getRoutes from './routes';
+import waitAll from './sagas/waitAll';
+import { Root } from 'containers';
+
+const app = new Express();
+const server = new http.Server(app);
+
+// disable `X-Powered-By` HTTP header
+app.disable('x-powered-by');
+
+app.use(compression());
+app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
+app.use(Express.static(path.join(__dirname, '..', 'static')));
+
+// Proxy to API
+app.use('/api', proxy(config.apiBaseUrl, {
+  // eslint-disable-next-line
+  forwardPath: (req, res) => url.parse(req.url).path
+}));
+
+app.use((req, res) => {
+  if (__DEVELOPMENT__) {
+    webpackIsomorphicTools.refresh();
+  }
+
+  const store = configureStore();
+  const allRoutes = getRoutes(store);
+  const assets = webpackIsomorphicTools.assets();
+
+  function hydrateOnClient() {
+    const htmlComponent = <Html assets={assets} store={store} />;
+    const renderedDomString = ReactDOMServer.renderToString(htmlComponent);
+    res.send(`<!doctype html>\n ${renderedDomString}`);
+  }
+
+  if (__DISABLE_SSR__) {
+    hydrateOnClient();
+    return;
+  }
+
+  const context = {};
+  const rootComponent = (
+    <Provider store={store}>
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        {allRoutes}
+      </StaticRouter>
+    </Provider>
+  );
+
+  const extractedRoutes = [];
+  function extractRoutes(routes) {
+    let r = routes;
+    if (_.isPlainObject(routes)) {
+      r = [routes];
+    }
+    _.forEach(r, (route) => {
+      extractedRoutes.push(route.props);
+
+      if (route.props.children) {
+        extractRoutes(route.props.children);
+      }
+    });
+  }
+  extractRoutes(allRoutes);
+
+  const filtered = [];
+  extractedRoutes.forEach((route) => {
+    const match = matchPath(req.url, {
+      path: route.path,
+      exact: true,
+      strict: false
+    });
+    if (match && (match.path !== '*')) {
+      filtered.push({match, component: route.component});
+    }
+  });//.map((route) => route.component);
+
+  console.log('filtered', filtered);
+  const preloaders = filtered
+    .filter(({component}) => component && component.preload)
+    .map(({match, component}) => component.preload(match.params, req))
+    .reduce((result, preloader) => result.concat(preloader), []);
+  const runTasks = store.runSaga(waitAll(preloaders));
+  global.navigator = { userAgent: req.headers['user-agent'] };
+
+  runTasks.done.then(() => {
+    if (context.url) {
+      res.redirect(context.url);
+    } else {
+      const htmlComponent = <Html assets={assets} component={rootComponent} store={store} />;
+      const renderedDomString = ReactDOMServer.renderToString(htmlComponent);
+      // console.log('renderedDomString', renderedDomString);
+      res.status(200).send(`<!doctype html>\n ${renderedDomString}`);
+      store.close();
+    }
+  }).catch((e) => {
+    console.log(e.stack);
+  });
+});
+
+if (config.port) {
+  server.listen(config.port, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.info('==> 💻  Open http://%s:%s in a browser to view the app.', config.host, config.port);
+  });
+} else {
+  console.error('==>     ERROR: No PORT environment variable has been specified');
+}
